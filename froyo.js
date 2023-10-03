578f7ba9b19a59d37d220582bddfce731e02c681
Ana Ruiz <aruiz_90@hotmail.com>
2023-10-02 19:31:05 -0500

    pseudo-code guide

--- froyo.js
+++ froyo.js
@@ -8,3 +8,20 @@ const userInputString = prompt(
 
   // Split the string of flavors into an array of flavors.
 const stringArray = userInputString.split(",");
+
+const iceCreamOrder = {
+    vanilla: 0,
+    chocolate: 0,
+    strawberry: 0
+}
+
+function printIceCreamOrder(stringArray) {
+    for(i = 0; i < stringArray.length; i++) {
+    //access the individual flavors
+    //increase the count of ice cream order depending on repetition of flavor
+    //adding 1 to flavor count 
+       
+    };
+    //once loop is complete, console.table order 
+    console.table(iceCreamOrder);
+};
